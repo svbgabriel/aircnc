a512b9bd2b37a8b1c2761cb19e9d5b73c844c3b1
Gabriel Batista <svbgabriel@gmail.com>
2019-11-24 18:21:20 -0300

    Configura o WebSocket

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
+
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-6r8bd.mongodb.net/semana09?retryWrites=true&w=majority",
@@ -14,9 +19,24 @@ mongoose.connect(
   }
 );
 
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
